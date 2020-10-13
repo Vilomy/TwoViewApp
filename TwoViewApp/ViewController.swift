acfc24ea6248a9c5c25ce7cd4c53aaf378fe2f2b
Gleb Zadonskiy <vilomy2@gmail.com>
2020-10-13 08:17:21 +0300

    Initial Commit

---
+++ TwoViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoViewApp
+//
+//  Created by Gleb Zadonskiy on 13.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
